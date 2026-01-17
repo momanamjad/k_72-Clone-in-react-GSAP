e0d6e3dc2192a4ecd2483a37887f980bdf869a90
MOMAN AMJAD <momanamjad07@gmail.com>
2026-01-17 15:43:26 +0500

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div>
       <div className="h-screen w-screen fixed">
-        <Video />
+        {/* <Video /> */}
       </div>
       <div className="h-screen w-screen relative flex flex-col justify-between">
         <HomeHerotext />
