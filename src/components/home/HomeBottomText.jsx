ace0c9d35c902f9dea1f44bbb9beec87601fbbea
MOMAN AMJAD <momanamjad07@gmail.com>
2026-01-19 16:07:39 +0500

    changes

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const HomeBottomText = () => {
   return <div className='flex font-[font2] justify-center select-none gap-10 pb-10 '> 
      <Link to="/projects" className='text-[6.5vw] leading-[6vw] border-3 border-white uppercase rounded-full px-14 py-1 pt-4 pb-3 hover:text-[#D3FD50] hover:border-[#D3FD50] select-none '>PROJECTS</Link>
-     <Link to="/agency" className='text-[6.5vw] leading-[6vw] border-3 border-white uppercase rounded-full px-14 py-1 pt-4 pb-3 hover:text-[#D3FD50]  hover:border-[#D3FD50] select-none'>AGENCE</Link>
+     <Link to="/agence" className='text-[6.5vw] leading-[6vw] border-3 border-white uppercase rounded-full px-14 py-1 pt-4 pb-3 hover:text-[#D3FD50]  hover:border-[#D3FD50] select-none'>AGENCE</Link>
   
   </div>
 };
