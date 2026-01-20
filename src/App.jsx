8e227dfa8460667753ee84714ad1d9ed1e7b5f98
MOMAN AMJAD <momanamjad07@gmail.com>
2026-01-20 12:47:05 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects' 
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const App = () => {
+  const stateParentRef=useRef(null);
+  useGSAP(function(){
+    const timeline=gsap.timeline();
+
+    timeline.to(stateParentRef.current,{
+      display: 'block',
+  })  
+     
+    timeline.from('.stare',{
+      height: 0,
+      stagger: {
+        amount: -0.25
+      },
+    })
+    timeline.to('.stare',{
+      y: '100%',  
+       stagger: {
+        amount: -0.25
+      },
+    })
+    timeline.to(stateParentRef.current,{
+      display: 'none',
+  })
+})
+
   return (
 <div className='text-white'>
- 
+ <div ref={stateParentRef} className=' stare h-screen w-full  top-0 fixed z-99'>
+  <div className=' stare h-full  w-full flex '>
+  <div className=' stare h-screen w-1/5 bg-black'>  </div>
+  <div className=' stare h-screen w-1/5 bg-black'>  </div>
+  <div className=' stare h-screen w-1/5 bg-black'>  </div>
+  <div className=' stare h-screen w-1/5 bg-black'>  </div>
+  <div className=' stare h-screen w-1/5 bg-black'>  </div>
+ </div>
+ </div>
   <Routes>
    
 <Route path="/" element={<Home />} />
