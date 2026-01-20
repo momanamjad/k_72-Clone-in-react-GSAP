73515fd8acf3366d31c4e851da109e53ee55e9c7
MOMAN AMJAD <momanamjad07@gmail.com>
2026-01-20 10:57:00 +0500

    changes

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -5,25 +5,51 @@ import React, { useRef } from "react";
 
 const Agence = () => {
   gsap.registerPlugin(ScrollTrigger);
-const imageDivRef=useRef(null);
-useGSAP(function() {
-  gsap.to(imageDivRef.current, 
-    {scrollTrigger:{
-      trigger:imageDivRef.current,
-      markers:true,
-      start:'top 28%',
-      end:'top -70%',
-      scrub:true,
-      pin:true
-    }
-    })
-})
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
+  const imageArray = [
+    "https://k72.ca/images/teamMembers/ChantalG_480x640.jpg?w=480&h=640&fit=crop&s=13093769c4a19cecd291ddcccd898991",
+    "https://k72.ca/images/teamMembers/CAMILLE_480X640_2.jpg?w=480&h=640&fit=crop&s=74317575b2d72fd11c5296615c383e4a",
+    "https://k72.ca/images/teamMembers/MEL_480X640.jpg?w=480&h=640&fit=crop&s=07c9bfee89816720b873e6748a276af6",
+    "https://k72.ca/images/teamMembers/MEGGIE_480X640_2.jpg?w=480&h=640&fit=crop&s=3604b19f8fc7b40f517954147698d847",
+    "https://k72.ca/images/teamMembers/Michele_480X640.jpg?w=480&h=640&fit=crop&s=ce85dc6d140947736baa739d0e59dab2",
+    "https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7",
+    "https://k72.ca/images/teamMembers/joel_480X640_3.jpg?w=480&h=640&fit=crop&s=1cadbf143b3aa916b1b414464acbb4d6",
+    "https://k72.ca/images/teamMembers/Olivier_480x640.jpg?w=480&h=640&fit=crop&s=c13569c0753117d04f1a93cf7b446d64",
+    "https://k72.ca/images/teamMembers/Jean-Philippe_480x640.jpg?w=480&h=640&fit=crop&s=2a3b4c5d6e7f8g9h0i1j2k3l4m5n6o7p",
+    "https://k72.ca/images/teamMembers/Marie-Eve_480x640.jpg?w=480&h=640&fit=crop&s=1f4f3f5f4e2e4d6b6c8e8f4e5d6c7b8a",
+  ];
+  useGSAP(function () {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        // markers:true,
+        start: "top 28%",
+        end: "top -70%",
+        scrub: true,
+        pin: true,
+        onUpdate: function (elem) {
+          let imageIndex;
+          if (elem.progress < 0.1) {
+            imageIndex = Math.floor(elem.progress * imageArray.length);
+          } else {
+            imageIndex = imageArray.length - 1;
+          }
+          imageRef.current.src = imageArray[imageIndex];
+        },
+      },
+    });
+  });
   return (
     <div>
       <div className="section1">
-        <div ref={imageDivRef} className="h-[20vw] rounded-4xl  w-[15vw] top-80 left-[30vw] overflow-hidden object-cover absolute">
+        <div
+          ref={imageDivRef}
+          className="h-[20vw] rounded-4xl  w-[15vw] top-80 left-[30vw] overflow-hidden object-cover absolute"
+        >
           <img
-             className="h-full w-full object-cover rounded-3xl"
+            ref={imageRef}
+            className="h-full w-full object-cover rounded-3xl"
             src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7"
             alt=""
           />
