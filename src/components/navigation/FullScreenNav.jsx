eb3594363f1ea1ac08881084efdce7a7f7083348
MOMAN AMJAD <momanamjad07@gmail.com>
2026-01-21 12:21:28 +0500

    changes

---
+++ src/components/navigation/FullScreenNav.jsx
@@ -0,0 +1,188 @@
+import React from "react";
+
+const FullScreenNav = () => {
+  return (
+    <div
+      id="fullscreen"
+      className="h-screen absolute text-white w-full py-40 overflow-x-hidden  bg-black"
+    >
+      <div className="">
+        <div className="link border-t  relative border-white ">
+          <h1 className="text-[8vw] uppercase leading-[1.2] text-center">
+            PROJECTS
+          </h1>
+          <div className="moveLink absolute flex top-0 bg-[#D3FD50] text-black">
+            <div className="moveX flex items-center">
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg"
+                alt=""
+              />
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+                alt=""
+              />
+            </div>
+            <div className="moveX flex items-center">
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg"
+                alt=""
+              />
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="link border-t  relative border-white ">
+          <h1 className="text-[8vw] uppercase leading-[1.2] text-center">
+            PROJECTS
+          </h1>
+          <div className="moveLink absolute flex top-0 bg-[#D3FD50] text-black">
+            <div className="moveX flex items-center">
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg"
+                alt=""
+              />
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+                alt=""
+              />
+            </div>
+            <div className="moveX flex items-center">
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg"
+                alt=""
+              />
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="link border-t  relative border-white ">
+          <h1 className="text-[8vw] uppercase leading-[1.2] text-center">
+            PROJECTS
+          </h1>
+          <div className="moveLink absolute flex top-0 bg-[#D3FD50] text-black">
+            <div className="moveX flex items-center">
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg"
+                alt=""
+              />
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+                alt=""
+              />
+            </div>
+            <div className="moveX flex items-center">
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg"
+                alt=""
+              />
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="link border-t  relative border-white ">
+          <h1 className="text-[8vw] uppercase leading-[1.2] text-center">
+            PROJECTS
+          </h1>
+          <div className="moveLink absolute flex top-0 bg-[#D3FD50] text-black">
+            <div className="moveX flex items-center">
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg"
+                alt=""
+              />
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+                alt=""
+              />
+            </div>
+            <div className="moveX flex items-center">
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg"
+                alt=""
+              />
+              <h2 className=" whitespace-nowrap text-[8vw] uppercase leading-[1.2] text-center">
+                TO SEE EVERYTHING
+              </h2>
+              <img
+                className="lg:h-36 h-14 rounded-full shrink-0 lg:w-96 w-32 object-cover"
+                src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+       
+      </div>
+    </div>
+  );
+};
+
+export default FullScreenNav;
