0508efee038f3c6908f3acb6d7fcdb5905e018da
MOMAN AMJAD <momanamjad07@gmail.com>
2026-02-11 19:12:17 +0500

    changes

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -65,7 +65,7 @@ const Agence = () => {
         <div className='relative font-[font2]'>
           <div className='lg:mt-[55vh] mt-[30vh]'>
             <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>Soixan7e <br />
-              Douze</h1>
+               </h1>
           </div>
           <div className='lg:pl-[40%] lg:mt-20 mt-4 p-3'>
             <p className='lg:text-6xl text-xl leading-tight'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
